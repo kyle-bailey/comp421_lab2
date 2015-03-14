07af6df76dcc8b8f48460d139219d0d305b597f9
Kyle <kyle.bailey@ymail.com>
2015-03-14 15:21:49 -0500

    forgot to include the .h

--- trap_handlers.c
+++ trap_handlers.c
@@ -1,3 +1,5 @@
+#import "trap_handler.h"
+
 void kernel_trap_handler(ExceptionStackFrame *frame) {
   TracePrintf(1, "Entering TRAP_KERNEL interrupt handler...\n");
   if(frame->code == YALNIX_GETPID){
--- trap_handlers.h
+++ trap_handlers.h
@@ -4,9 +4,15 @@
 #include "process_control_block.h"
 
 void kernel_trap_handler(ExceptionStackFrame *frame);
+
 void clock_trap_handler (ExceptionStackFrame *frame);
+
 void illegal_trap_handler (ExceptionStackFrame *frame);
+
 void memory_trap_handler (ExceptionStackFrame *frame);
+
 void math_trap_handler (ExceptionStackFrame *frame);
+
 void tty_recieve_trap_handler (ExceptionStackFrame *frame);
+
 void tty_transmit_trap_handler (ExceptionStackFrame *frame);
