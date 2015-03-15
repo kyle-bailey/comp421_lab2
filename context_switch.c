6f38438a0417f316ed7fddf1da5232fad7d839cb
aaronroe <aaronalkan@gmail.com>
2015-03-14 19:58:16 -0500

    Fix idle_and_init_initialization kernel stack physical page copying

--- context_switch.c
+++ context_switch.c
@@ -23,6 +23,7 @@ context_switch_helper(SavedContext *ctxp, void *p1, void *p2){
 SavedContext *
 idle_and_init_initialization(SavedContext *ctxp, void *p1, void *p2) {
   int i = 0;
+  int j = 0;
 
   struct process_control_block *pcb1 = (struct process_control_block *)p1;
   struct process_control_block *pcb2 = (struct process_control_block *)p2;
@@ -34,26 +35,45 @@ idle_and_init_initialization(SavedContext *ctxp, void *p1, void *p2) {
   TracePrintf(3, "context_switch: beginning kernel stack copy.\n");
 
   for (i = 0; i < KERNEL_STACK_PAGES; i++) {
-    TracePrintf(4, "context_switch: 0Entering %d\n", i);
     unsigned int process_2_physical_page_number = acquire_free_physical_page();
-    TracePrintf(4, "context_switch: 1Entering %d\n", i);
-    void *process_2_physical_page = (void *)(long)(process_2_physical_page_number  * PAGESIZE + PMEM_BASE);
-    TracePrintf(4, "context_switch: 2Entering %d\n", i);
-    void *process_1_physical_page = (void *)(long)((process_1_page_table[i + KERNEL_STACK_BASE/PAGESIZE].pfn * PAGESIZE) + PMEM_BASE);
-    TracePrintf(4, "context_switch: 3Entering %d\n", i);
-    TracePrintf(4, "context_switch: process 2: %p\n", process_2_physical_page);
-    TracePrintf(4, "context_switch: process 1: %p\n", process_1_physical_page);
-    memcpy(
-      process_2_physical_page, // dest
-      process_1_physical_page, // src
-      PAGESIZE
-    );
-    TracePrintf(4, "context_switch: 4Entering %d\n", i);
 
-    process_2_page_table[i + KERNEL_STACK_BASE/PAGESIZE].pfn = process_2_physical_page_number;
-    process_2_page_table[i + KERNEL_STACK_BASE/PAGESIZE].valid = 1;
-    process_2_page_table[i + KERNEL_STACK_BASE/PAGESIZE].kprot = process_1_page_table[i + KERNEL_STACK_BASE/PAGESIZE].kprot;
-    process_2_page_table[i + KERNEL_STACK_BASE/PAGESIZE].uprot = process_1_page_table[i + KERNEL_STACK_BASE/PAGESIZE].uprot;
+    for (j = MEM_INVALID_PAGES; j < KERNEL_STACK_BASE; j++) {
+      // find the first available page from the user's heap.
+      if (process_1_page_table[j].valid == 0) {
+        // temporarily map that page to a physical page.
+        process_1_page_table[j].valid = 1;
+        process_1_page_table[j].kprot = PROT_READ | PROT_WRITE;
+        process_1_page_table[j].uprot = PROT_READ | PROT_EXEC;
+        process_1_page_table[j].pfn = process_2_physical_page_number;
+
+        void *process_1_virtual_addr = (void *)(long)(((KERNEL_STACK_BASE/PAGESIZE + i) * PAGESIZE) + PMEM_BASE);
+        void *temp_virt_addr_for_kernel_stack = (void *)(long)((j * PAGESIZE) + PMEM_BASE);
+
+        WriteRegister(REG_TLB_FLUSH, (RCS421RegVal) temp_virt_addr_for_kernel_stack);
+
+        TracePrintf(4, "context_switch: %p\n", process_1_virtual_addr);
+        TracePrintf(4, "context_switch: %p\n", temp_virt_addr_for_kernel_stack);
+
+        memcpy(
+          temp_virt_addr_for_kernel_stack, // dest
+          process_1_virtual_addr, // src
+          PAGESIZE
+        );
+
+        // pretend that the temp memory never existed.
+        process_1_page_table[j].valid = 0;
+        WriteRegister(REG_TLB_FLUSH, (RCS421RegVal) temp_virt_addr_for_kernel_stack);
+
+        // give the pfn from the temp memory to process 2's page table.
+        process_2_page_table[i + KERNEL_STACK_BASE/PAGESIZE].valid = 1;
+        process_2_page_table[i + KERNEL_STACK_BASE/PAGESIZE].kprot = process_1_page_table[i + KERNEL_STACK_BASE/PAGESIZE].kprot;
+        process_2_page_table[i + KERNEL_STACK_BASE/PAGESIZE].uprot = process_1_page_table[i + KERNEL_STACK_BASE/PAGESIZE].uprot;
+        process_2_page_table[i + KERNEL_STACK_BASE/PAGESIZE].pfn = process_2_physical_page_number;
+        break;
+      }
+    }
+
+    TracePrintf(4, "context_switch: 4Entering %d\n", i);
   }
 
   TracePrintf(1, "context_switch: idle_and_init_initialization completed.\n");
